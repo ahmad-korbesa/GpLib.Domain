ec7f9b1f7709b96e41ef6520498e774c7ef62368
ahmad.korbesa <ahmad.korbesa@neowave.com.my>
2020-10-21 06:17:52 +0200

    added timestamp

--- GpLib.Domain.Abstractions/DomainEvent.cs
+++ GpLib.Domain.Abstractions/DomainEvent.cs
@@ -8,11 +8,17 @@ namespace GpLib.Domain.Abstractions
         protected DomainEvent(TAggregateKey aggregateId, Guid guid, int version)
         {
             AggregateId = aggregateId;
+
+            Timestamp = DateTime.Now;
+            
             Guid = guid;
+            
             Version = version;
         }
 
         public TAggregateKey AggregateId { get; protected set; }
+
+        public DateTime Timestamp { get; protected set; }
 
         public int Version { get; protected set; }
 
