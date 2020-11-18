3235408237f43fc59c33880be1cfebb6c99e7938
ahmad.korbesa <ahmad.korbesa@neowave.com.my>
2020-11-18 21:10:27 +0100

    revert to dotnet core 3

--- GpLib.Domain.Abstractions/DomainEvent.cs
+++ GpLib.Domain.Abstractions/DomainEvent.cs
@@ -2,8 +2,26 @@
 
 namespace GpLib.Domain.Abstractions
 {
-    public abstract record DomainEvent<TAggregateKey>
-        (TAggregateKey AggregateId, DateTime Timestamp, int Version, Guid EventId)
+    //public abstract record DomainEvent<TAggregateKey>
+    //    (TAggregateKey AggregateId, DateTime Timestamp, int Version, Guid EventId)
+    //{
+    //}
+
+    public abstract class DomainEvent<TAggregateKey>
     {
+        protected DomainEvent(TAggregateKey aggregateId, DateTime timestamp, int version, Guid eventId)
+        {
+            AggregateId = aggregateId;
+            Timestamp = timestamp;
+            Version = version;
+            EventId = eventId;
+        }
+
+        public TAggregateKey AggregateId { get; }
+        public DateTime Timestamp { get; }
+        public int Version { get; }
+        public Guid EventId { get; }
     }
+
+
 }
--- GpLib.Domain.Tests/Events/ValueAdded2.cs
+++ GpLib.Domain.Tests/Events/ValueAdded2.cs
@@ -3,9 +3,20 @@ using System;
 
 namespace GpLib.Domain.Tests
 {
-    public record ValueAdded2
-        (double Value, Guid AggregateId, Guid EventId, DateTime Timestanp)
-        : DomainEvent<Guid>(AggregateId, Timestanp, 0, EventId)
+    //public record ValueAdded2
+    //    (double Value, Guid AggregateId, Guid EventId, DateTime Timestanp)
+    //    : DomainEvent<Guid>(AggregateId, Timestanp, 0, EventId)
+    //{
+    //}
+
+    public class ValueAdded2 : DomainEvent<Guid>
     {
+        public ValueAdded2(double value, Guid aggregateId, Guid eventId, DateTime timestamp) : base(aggregateId, timestamp, 0, eventId)
+        {
+            Value = value;
+        }
+
+        public double Value { get; }
     }
+
 }
